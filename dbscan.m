1a539c39c09f73fdec90c669e89e3f17a0ae0767
YE ZHU <19607512+zhuye88@users.noreply.github.com>
2021-04-26 22:22:44 +1000

    Update dbscan.m

--- dbscan.m
+++ dbscan.m
@@ -1,4 +1,4 @@
-function [ class ] = DBSCAN(dis, eps, minPts)
+function [ class ] = dbscan(dis, eps, minPts)
 
 % max_dis = max(max(dis)); % normalise dis matrix
 % if max_dis > 1
@@ -9,9 +9,8 @@ m_adjacency = dis <= eps;
 l_core = sum(m_adjacency, 2) >= minPts;
 m_adjacency_core = m_adjacency(l_core, l_core);
 
-class = zeros(size(dis, 1), 1)-1;
-
 if isempty(m_adjacency_core)
+    class = zeros(size(dis, 1), 1)-1;
     return % No core, all noise
 end
 
@@ -21,7 +20,6 @@ g_dbscan = graph(gather(m_adjacency_core));
 class_core = conncomp(g_dbscan);
 
 [l_assign_dis, l_assign_core] = min(dis(l_core, :));
-
 class = class_core(l_assign_core);
 class(l_assign_dis > eps) = -1;
 
